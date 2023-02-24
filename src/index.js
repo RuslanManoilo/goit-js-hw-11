e7a2ad20ecc3c09219ac2609a479a754057f6c3f
Ruslan <manoiloruslan@gmail.com>
2023-02-25 00:30:40 +0200

    fix function createImgList

--- src/index.js
+++ src/index.js
@@ -17,8 +17,10 @@ let page = 1;
 form.addEventListener('submit', onSubmit);
 loadMoreBtn.addEventListener('click', onLoad);
 
+
 function onSubmit(event) {
     event.preventDefault();
+
     gallery.innerHTML = "";
     page = 1;
 
@@ -26,10 +28,8 @@ function onSubmit(event) {
     searchConstructor().then(totalHits => {
         console.log(totalHits);
         if (totalHits === 0) {
-            // loadMoreBtn.hidden = true;
+            loadMoreBtn.hidden = true;         
             gallery.innerHTML = "";
-            
-            Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
         };
 
         if (totalHits > 0) {
@@ -38,6 +38,7 @@ function onSubmit(event) {
         };
     }).catch(error => console.log(error));
 
+    // event.currentTarget.reset();
 };
 
 
@@ -71,27 +72,31 @@ async function searchConstructor() {
 
 
 function createImgList(listImg) {
-    const markupImg = listImg.map(item => {
-    return `<a href="${item.largeImageURL}">
-                <div class="photo-card">
-                    <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-                    <div class="info">
-                        <p class="info-item">
-                            <b>Likes ${item.likes}</b>
-                        </p>
-                        <p class="info-item">
-                            <b>Views ${item.views}</b>
-                        </p>
-                        <p class="info-item">
-                            <b>Comments ${item.comments}</b>
-                        </p>
-                        <p class="info-item">
-                            <b>Downloads ${item.downloads}</b>
-                        </p>
-                    </div>
-                </div>
-            </a>`}).join('');
-
-    gallery.insertAdjacentHTML('beforeend', markupImg);
-    lightbox.refresh();
+    if (listImg.length > 0) {
+        const markupImg = listImg.map(item => {
+            return `<a href="${item.largeImageURL}">
+                        <div class="photo-card">
+                            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+                            <div class="info">
+                                <p class="info-item">
+                                    <b>Likes ${item.likes}</b>
+                                </p>
+                                <p class="info-item">
+                                    <b>Views ${item.views}</b>
+                                </p>
+                                <p class="info-item">
+                                    <b>Comments ${item.comments}</b>
+                                </p>
+                                <p class="info-item">
+                                    <b>Downloads ${item.downloads}</b>
+                                </p>
+                            </div>
+                        </div>
+                    </a>`}).join('');
+        
+        gallery.insertAdjacentHTML('beforeend', markupImg);
+        lightbox.refresh(); 
+    } else {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    };
 };
