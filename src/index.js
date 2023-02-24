7a68bd7e98427e46dc054df340ba9db7617aff88
Ruslan <manoiloruslan@gmail.com>
2023-02-24 23:49:35 +0200

    fix lightbox

--- src/index.js
+++ src/index.js
@@ -71,28 +71,27 @@ async function searchConstructor() {
 
 
 function createImgList(listImg) {
-    const markupImg = listImg.map(item => 
-    `<div class="photo-card">
-        <a href="${item.largeImageURL}">
-            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-        </a>
-        <div class="info">
-            <p class="info-item">
-                <b>Likes ${item.likes}</b>
-            </p>
-            <p class="info-item">
-                <b>Views ${item.views}</b>
-            </p>
-            <p class="info-item">
-                <b>Comments ${item.comments}</b>
-            </p>
-            <p class="info-item">
-                <b>Downloads ${item.downloads}</b>
-            </p>
-        </div>
-    </div>`).join('');
+    const markupImg = listImg.map(item => {
+    return `<a href="${item.largeImageURL}">
+                <div class="photo-card">
+                    <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes ${item.likes}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Views ${item.views}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Comments ${item.comments}</b>
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads ${item.downloads}</b>
+                        </p>
+                    </div>
+                </div>
+            </a>`}).join('');
 
+    gallery.insertAdjacentHTML('beforeend', markupImg);
     lightbox.refresh();
-
-    return gallery.insertAdjacentHTML('beforeend', markupImg);
 };
