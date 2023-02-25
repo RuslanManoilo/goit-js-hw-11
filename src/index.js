feb76299c3bd0ad5dc8555cdaaa739f7cad1e2cb
Ruslan <manoiloruslan@gmail.com>
2023-02-25 23:12:40 +0200

    fix import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { pixabayAPI } from "./fetchPixabayAPI";
-import { searchConstructor } from "./fetchPixabayAPI";
 import { createImgList } from "./createMarkup";
 import axios from "axios";
 import Notiflix from 'notiflix';
@@ -55,7 +53,27 @@ function onLoad() {
             Notiflix.Notify.info(`We're sorry, but you've reached the end of search results.`);
         }
    });
-}
+};
+
+
+async function pixabayAPI() {
+    const KEY = '33878200-75945f3143f242bd251e2a138';
+    const BASE_URL = 'https://pixabay.com/api/';
+
+    const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${searchInput.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+
+    return response.data;
+};
+
+
+async function searchConstructor() {
+    const constructor = await pixabayAPI(searchInput.value).then(data => {
+        createImgList(data.hits);
+        return data.totalHits;
+    });
+
+    return constructor;
+};
 
 
 loadMoreBtn.classList.add('visually-hidden');
